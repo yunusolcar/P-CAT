e3d0bc1f425260b409662d939b5446ed68fc2b1c
Yunus <yunusolcar@hotmail.com>
2023-05-08 16:22:00 +0300

    Pcat - Add Photo

--- app.js
+++ app.js
@@ -8,15 +8,21 @@ app.set('view engine', 'ejs');
 
 //Middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.render('index');
+     res.render('index');
 });
 app.get('/about', (req, res) => {
-  res.render('about');
+     res.render('about');
 });
 app.get('/add', (req, res) => {
-  res.render('add');
+     res.render('add');
+});
+app.post('/photos', (req, res) => {
+     console.log(req.body);
+     res.redirect('/');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
