8171d742ea28635619b92d777cb507a23974047a
Yunus <yunusolcar@hotmail.com>
2023-05-11 22:18:02 +0300

    Clean Blog - Upload Image

--- app.js
+++ app.js
@@ -3,32 +3,34 @@ const path = require('path');
 const app = express();
 const mongoose = require('mongoose');
 const Photo = require('./models/Photo');
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
 
 //Connect Db
 mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db');
 
-//Template
+//Template Engine
 app.set('view engine', 'ejs');
 
-//Middleware
+//Middlewares
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
+//Routers
 app.get('/', async (req, res) => {
-     const photos = await Photo.find({});
+     const photos = await Photo.find({}).sort('-dateCreated');
      res.render('index', {
           photos,
      });
 });
 
 app.get('/photos/:id', async (req, res) => {
-     //console.log(req.params.id)
-     // res.render('about');
-     const photo =await Photo.findById(req.params.id);
-     res.render('photo',{
-          photo
-     })
+     const photo = await Photo.findById(req.params.id);
+     res.render('photo', {
+          photo,
+     });
 });
 
 app.get('/about', (req, res) => {
@@ -40,8 +42,21 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-     await Photo.create(req.body);
-     res.redirect('/');
+
+     const uploadDir = 'public/uploads';
+     if (!fs.existsSync(uploadDir)) {
+          fs.mkdirSync(uploadDir);
+     }
+
+     let uploadedImage = req.files.image;
+     let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
+     uploadedImage.mv(uploadPath, async () => {
+          await Photo.create({
+               ...req.body,
+               image: '/uploads/' + uploadedImage.name,
+          });
+          res.redirect('/');
+     });
 });
 
 //Port
