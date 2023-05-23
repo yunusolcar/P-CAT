ca47a90074fbae233cfd34f192e5b4957ee8ec44
Yunus <yunusolcar@hotmail.com>
2023-05-23 11:25:54 +0300

    Added Photo model and schema for MongoDB

--- models/Photo.js
+++ models/Photo.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db');
-
 const PhotoSchema = new Schema({
      title: String,
      description: String,
