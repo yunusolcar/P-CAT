e3e1ba0d8490368684a9455d5e83aa86bf1100fd
Yunus <yunusolcar@hotmail.com>
2023-05-24 11:19:01 +0300

    photo controllers

--- controllers/photoControllers.js
+++ controllers/photoControllers.js
@@ -43,7 +43,9 @@ exports.createPhoto = async (req, res) => {
 };
 
 exports.updatePhoto = async (req, res) => {
-     const photo = await Photo.findOne({ _id: req.params.id });
+     const photo = await Photo.findOne({
+          _id: req.params.id
+     });
      photo.title = req.body.title;
      photo.description = req.body.description;
      photo.save();
@@ -51,7 +53,9 @@ exports.updatePhoto = async (req, res) => {
 };
 
 exports.deletePhoto = async (req, res) => {
-     const photo = await Photo.findOne({ _id: req.params.id });
+     const photo = await Photo.findOne({
+          _id: req.params.id
+     });
      let deletedImage = __dirname + '/../public' + photo.image;
      fs.unlinkSync(deletedImage);
      await Photo.findByIdAndRemove(req.params.id);
